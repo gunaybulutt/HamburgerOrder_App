4e6b556d45f57495a2880cdd4c74a7e809e2d85e
Gunay Bulut <gunaybulut.gc@gmail.com>
2023-11-25 18:06:36 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace HamburgerOrder
+{
+    partial class HamburgerOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hamburgerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InfoBox = new System.Windows.Forms.GroupBox();
+            this.priceText = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sauseText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.potatoTypeText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.beverageTypeText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.beverageNameText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuNameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Add = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.InfoBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hamburgerToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1051, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hamburgerToolStripMenuItem
+            // 
+            this.hamburgerToolStripMenuItem.Name = "hamburgerToolStripMenuItem";
+            this.hamburgerToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.hamburgerToolStripMenuItem.Text = " Hamburger Order";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // InfoBox
+            // 
+            this.InfoBox.BackColor = System.Drawing.Color.Silver;
+            this.InfoBox.Controls.Add(this.button1);
+            this.InfoBox.Controls.Add(this.priceText);
+            this.InfoBox.Controls.Add(this.label6);
+            this.InfoBox.Controls.Add(this.sauseText);
+            this.InfoBox.Controls.Add(this.label5);
+            this.InfoBox.Controls.Add(this.potatoTypeText);
+            this.InfoBox.Controls.Add(this.label4);
+            this.InfoBox.Controls.Add(this.beverageTypeText);
+            this.InfoBox.Controls.Add(this.label3);
+            this.InfoBox.Controls.Add(this.beverageNameText);
+            this.InfoBox.Controls.Add(this.label2);
+            this.InfoBox.Controls.Add(this.menuNameText);
+            this.InfoBox.Controls.Add(this.label1);
+            this.InfoBox.Location = new System.Drawing.Point(12, 38);
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.Size = new System.Drawing.Size(844, 277);
+            this.InfoBox.TabIndex = 1;
+            this.InfoBox.TabStop = false;
+            this.InfoBox.Text = "HamburgerInfo";
+            // 
+            // priceText
+            // 
+            this.priceText.Location = new System.Drawing.Point(557, 123);
+            this.priceText.Name = "priceText";
+            this.priceText.Size = new System.Drawing.Size(168, 23);
+            this.priceText.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(428, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Price";
+            // 
+            // sauseText
+            // 
+            this.sauseText.Location = new System.Drawing.Point(557, 82);
+            this.sauseText.Name = "sauseText";
+            this.sauseText.Size = new System.Drawing.Size(168, 23);
+            this.sauseText.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Extrasauce";
+            // 
+            // potatoTypeText
+            // 
+            this.potatoTypeText.Location = new System.Drawing.Point(557, 42);
+            this.potatoTypeText.Name = "potatoTypeText";
+            this.potatoTypeText.Size = new System.Drawing.Size(168, 23);
+            this.potatoTypeText.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Silver;
+            this.label4.Location = new System.Drawing.Point(428, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Potato Type";
+            // 
+            // beverageTypeText
+            // 
+            this.beverageTypeText.Location = new System.Drawing.Point(135, 124);
+            this.beverageTypeText.Name = "beverageTypeText";
+            this.beverageTypeText.Size = new System.Drawing.Size(168, 23);
+            this.beverageTypeText.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Beverage Type";
+            // 
+            // beverageNameText
+            // 
+            this.beverageNameText.Location = new System.Drawing.Point(135, 77);
+            this.beverageNameText.Name = "beverageNameText";
+            this.beverageNameText.Size = new System.Drawing.Size(168, 23);
+            this.beverageNameText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Beverage Name";
+            // 
+            // menuNameText
+            // 
+            this.menuNameText.Location = new System.Drawing.Point(135, 39);
+            this.menuNameText.Name = "menuNameText";
+            this.menuNameText.Size = new System.Drawing.Size(168, 23);
+            this.menuNameText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 358);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1027, 268);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.Lime;
+            this.Add.Location = new System.Drawing.Point(901, 38);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(117, 65);
+            this.Add.TabIndex = 12;
+            this.Add.Text = "ADD";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(901, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 64);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(901, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 64);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "UPDATE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(726, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // HamburgerOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1051, 655);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.InfoBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "HamburgerOrder";
+            this.Text = "HamurgerOrder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.InfoBox.ResumeLayout(false);
+            this.InfoBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hamburgerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox InfoBox;
+        private System.Windows.Forms.TextBox beverageNameText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox menuNameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox beverageTypeText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox priceText;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox sauseText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox potatoTypeText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HamburgerOrder
+{
+    public partial class HamburgerOrder : Form
+    {
+
+        SqlConnection connection = new SqlConnection("Data Source");  //enter your own data source
+
+        public HamburgerOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            show();
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            String menuName = menuNameText.Text;
+            String beverageName = beverageNameText.Text;
+            String beverageType = beverageTypeText.Text;
+            String potatoType = potatoTypeText.Text;
+            String sause = sauseText.Text;
+            String price = priceText.Text;
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO ORDERS(MenuName,BeverageName,BeverageType,PotatoType,Extrasauce,Price) " +
+                    "VALUES('" + menuName + "','" + beverageName + "','" + beverageType + "','" + potatoType + "','" + sause + "','" + price + "'  )", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                show();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+            
+
+        }
+
+        private void show()
+        {
+            connection.Open();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from ORDERS",connection);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            connection.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            menuNameText.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            beverageNameText.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            beverageTypeText.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            potatoTypeText.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            sauseText.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            priceText.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//delete
+
+            try
+            {
+                connection.Open();
+                String id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                SqlCommand command = new SqlCommand("DELETE FROM ORDERS WHERE ID=('" + id + "')", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                show();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {// update
+
+            try
+            {
+
+                String menuName = menuNameText.Text;
+                String beverageName = beverageNameText.Text;
+                String beverageType = beverageTypeText.Text;
+                String potatoType = potatoTypeText.Text;
+                String sause = sauseText.Text;
+                String price = priceText.Text;
+                String id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE ORDERS SET MenuName='" + menuName+ "',BeverageName='" + beverageName + "',BeverageType='" + beverageType +
+                  "',PotatoType='" + potatoType + "',Extrasauce='" + sause+ "',Price='" + price+"' WHERE ID='"+id+"' " , connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                show();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//clear
+            menuNameText.Text = "";
+            beverageNameText.Text = "";
+            beverageTypeText.Text = "";
+            potatoTypeText.Text = "";
+            sauseText.Text = "";
+            priceText.Text = "";
+        }
+    }
+}
